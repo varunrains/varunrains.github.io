6eb493d6e2d0e1048f17ea0896479c0efa65211f
Varun Upadhya <varun.upadhya1@gmail.com>
2025-03-17 15:52:38 +0530

    updated contact details

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1742203315',resources: [
+    cacheName: 'chirpy-1742206947',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
